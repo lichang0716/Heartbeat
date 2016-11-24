67644fbaeda9b74cad443c1dd681391a708495a1
lichang0716 <lichang.android@gmail.com>
2016-11-24 22:44:50 +0800

    初始化仓库，添加 AppIcon

---
+++ Heartbeat/Heartbeat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Heartbeat
+//
+//  Created by pwnlc on 2016/11/24.
+//  Copyright © 2016年 xyz.pwnlc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Heartbeat/Heartbeat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Heartbeat
+//
+//  Created by pwnlc on 2016/11/24.
+//  Copyright © 2016年 xyz.pwnlc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
